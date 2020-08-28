b4540f322aeda08b86dd8dac4a7de1116519f6bc
Steven Waldron <steven.waldron@pm.me>
2020-08-28 09:41:10 -0500

    Removed unneeded maps

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,14 +1,11 @@
 use std::collections::HashSet;
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let mut set = HashSet::new();
-
     sentence
+        .to_ascii_lowercase()
         .chars()
         .filter(char::is_ascii_alphabetic)
-        .map(|c| c.to_ascii_lowercase())
-        .map(|c| set.insert(c))
-        .filter(|b| *b)
-        .count()
+        .collect::<HashSet<char>>()
+        .len()
         == 26
 }
