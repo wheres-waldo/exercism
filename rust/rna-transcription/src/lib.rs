1450e26542b6664d69f96997d77662f476114556
Steven Waldron <steven.waldron@pm.me>
2020-08-31 10:05:56 -0500

    Solution to rna-transcription problem

---
+++ rust/rna-transcription/src/lib.rs
@@ -0,0 +1,46 @@
+const ACGT: &str = "ACGT";
+const ACGU: &str = "ACGU";
+
+#[derive(Debug, PartialEq)]
+pub struct DNA(String);
+
+#[derive(Debug, PartialEq)]
+pub struct RNA(String);
+
+impl DNA {
+    pub fn new(dna: &str) -> Result<DNA, usize> {
+        Ok(DNA {
+            0: dna
+                .char_indices()
+                .map(|(i, c)| if ACGT.contains(c) { Ok(c) } else { Err(i) })
+                .collect::<Result<String, usize>>()?,
+        })
+    }
+
+    pub fn into_rna(self) -> RNA {
+        RNA {
+            0: self.0.chars().map(self::dna_to_rna).collect(),
+        }
+    }
+}
+
+impl RNA {
+    pub fn new(rna: &str) -> Result<RNA, usize> {
+        Ok(RNA {
+            0: rna
+                .char_indices()
+                .map(|(i, c)| if ACGU.contains(c) { Ok(c) } else { Err(i) })
+                .collect::<Result<String, usize>>()?,
+        })
+    }
+}
+
+fn dna_to_rna(nucleotide: char) -> char {
+    match nucleotide {
+        'A' => 'U',
+        'C' => 'G',
+        'G' => 'C',
+        'T' => 'A',
+        _ => panic!("Oops, how did we get here? {}", nucleotide),
+    }
+}
