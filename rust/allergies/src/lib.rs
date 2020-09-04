4730713dfa48cd3275eeb7501148cbf923f5c92b
Steven Waldron <steven.waldron@pm.me>
2020-09-04 14:07:48 -0500

    Better solution Turns out you can assign a value to enums

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,27 +1,6 @@
-use lazy_static::lazy_static;
-use std::collections::BTreeMap;
 use Allergen::*;
 
-lazy_static! {
-    static ref ALLERGIES: BTreeMap<Allergen, u8> = [
-        (Eggs, 0),
-        (Peanuts, 1),
-        (Shellfish, 2),
-        (Strawberries, 3),
-        (Tomatoes, 4),
-        (Chocolate, 5),
-        (Pollen, 6),
-        (Cats, 7)
-    ]
-    .iter()
-    .cloned()
-    .collect();
-}
-
-pub struct Allergies(u32);
-
-#[derive(Debug, Clone, Copy, Eq, Ord, PartialOrd, PartialEq)]
-pub enum Allergen {
+const ALLERGIES: [Allergen; 8] = [
     Eggs,
     Peanuts,
     Shellfish,
@@ -30,6 +9,20 @@ pub enum Allergen {
     Chocolate,
     Pollen,
     Cats,
+];
+
+pub struct Allergies(u32);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
 }
 
 impl Allergies {
@@ -38,12 +31,12 @@ impl Allergies {
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        self.0 & (1 << ALLERGIES.get(allergen).unwrap()) != 0
+        self.0 & *allergen as u32 != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
         ALLERGIES
-            .keys()
+            .iter()
             .cloned()
             .filter(|a| self.is_allergic_to(a))
             .collect()
